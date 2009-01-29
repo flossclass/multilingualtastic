aa6f2f25670d7c98f59d2b0f56aa57f21b15ed63
Seth Viebrock <seth2@viebrock.us>
2009-01-29 01:03:44 -0600

    add files.py/.rb

---
+++ python/2_files.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+a_string = 'This is a string. Wow!\n'
+another_string = 'Whoa, this is another string!\n'
+
+file_location = '2_files/temp.txt'
+
+#You must open and close data streams (aka "file handles") to files:
+#THINK: What is `output`?
+#ANSWER: It is a "file object". It's not the file itself. It represents the file.
+output = open( file_location , 'w' ) #`w` means "write mode"
+output.write( a_string )
+output.write( another_string )
+#The filehandle will not close unless the parentheses are present on `close`:
+output.close() 
+
+print "This is what's in the file so far:"
+
+input = open( file_location, 'r' ) #`r` means "read mode"
+for file_line in input:
+    print file_line
+input.close()
+
